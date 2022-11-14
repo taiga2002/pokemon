1d6c9a383019114e1cda7fe42239f5118bde18ac
Taiga Kitao <taiga20022002@gmail.com>
2022-11-14 12:29:37 -0800

    Initial Commit

---
+++ PokedexSwiftUITutorial/PokedexSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexSwiftUITutorialApp.swift
+//  PokedexSwiftUITutorial
+//
+//  Created by 北尾　大河 on 2022/11/14.
+//
+
+import SwiftUI
+
+@main
+struct PokedexSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
